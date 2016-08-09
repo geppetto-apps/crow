c48afcc9a8f8fcf59e203116fd88329dcac387be
Theodor Tonum <theodor@tonum.no>
2016-08-09 18:39:23 +0200

    Some basic hello world examples

--- spec/cr2flow_spec.cr
+++ spec/cr2flow_spec.cr
@@ -1,9 +1,67 @@
 require "./spec_helper"
 
 describe Cr2flow do
-  # TODO: Write tests
+  it "should work with hello world" do
+    crystal = <<-CRYSTAL
+    p "Hello World"
+    CRYSTAL
 
-  it "works" do
-    false.should eq(true)
+    flow = <<-FLOW
+    console.log("Hello World");
+    FLOW
+
+    Cr2flow.convert(crystal).should eq flow
+  end
+
+  it "should work with string assignment" do
+    crystal = <<-CRYSTAL
+    text = "Hello World"
+    p text
+    CRYSTAL
+
+    flow = <<-FLOW
+    const text = "Hello World";
+    console.log(text);
+    FLOW
+
+    Cr2flow.convert(crystal).should eq flow
+  end
+
+  it "should work with concatenating strings" do
+    crystal = <<-CRYSTAL
+    p "Hello" + "World"
+    CRYSTAL
+
+    flow = <<-FLOW
+    console.log("Hello" + "World");
+    FLOW
+
+    Cr2flow.convert(crystal).should eq flow
+  end
+
+  it "should work with interpolated string" do
+    crystal = <<-CRYSTAL
+    who = "World"
+    p "Hello \#{who}"
+    CRYSTAL
+
+    flow = <<-FLOW
+    const who = "World";
+    console.log(`Hello ${who}`);
+    FLOW
+
+    Cr2flow.convert(crystal).should eq flow
+  end
+
+  it "should transpile arithmetic operations properly" do
+    crystal = <<-CRYSTAL
+    p 512 * 16 + 24 - 12
+    CRYSTAL
+
+    flow = <<-FLOW
+    console.log((512 * 16) + 24 - 12);
+    FLOW
+
+    Cr2flow.convert(crystal).should eq flow
   end
 end
--- src/cr2flow.cr
+++ src/cr2flow.cr
@@ -1,5 +1,61 @@
 require "./cr2flow/*"
+require "crystal/compiler/crystal/syntax.cr"
 
 module Cr2flow
-  # TODO Put your code here
+  def self.convert(crystal_source_code)
+    parser = Crystal::Parser.new(crystal_source_code)
+    node = Crystal::Expressions.from(parser.parse)
+    transpile node
+  end
+
+  private def self.transpile(content : String)
+    content
+  end
+
+  private def self.transpile(node : Crystal::Expressions)
+    node.expressions.map do |node|
+      transpile node
+    end.join("\n")
+  end
+
+  private def self.transpile(node : Crystal::ASTNode)
+    transpile node.to_s
+  end
+
+  private def self.transpile(node : Crystal::StringInterpolation)
+    data = node.expressions.map do |node|
+      case node
+      when Crystal::StringLiteral
+        node.value
+      else
+        "${#{transpile node}}"
+      end
+    end
+    "`#{data.join}`"
+  end
+
+  private def self.transpile(node : Crystal::Assign)
+    "const " + transpile node.to_s + ";"
+  end
+
+  private def self.transpile(call : Crystal::Call)
+    method = call.name
+    case call.name
+    when "+"
+      "#{call.obj} + #{call.args[0]}"
+    when "-"
+      "#{call.obj} - #{call.args[0]}"
+    when "*"
+      "#{call.obj} * #{call.args[0]}"
+    when "/"
+      "#{call.obj} / #{call.args[0]}"
+    else
+      args = call.args.map do |arg|
+        transpile arg
+      end
+
+      method = "console.log" if method == "p"
+      "#{method}(#{args.join(", ")});"
+    end
+  end
 end
