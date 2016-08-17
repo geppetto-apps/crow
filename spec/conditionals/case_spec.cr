94ab5362da0eed63334baa5e47f362350c2ca291
Theodor Tonum <theodor@tonum.no>
2016-08-17 13:06:04 +0200

    Add basic transpilation for case/switch statements

---
+++ spec/conditionals/case_spec.cr
@@ -0,0 +1,61 @@
+require "../spec_helper"
+
+describe Crow do
+  it "should convert simple case statements" do
+    crystal = <<-CR
+    value = "foo"
+    case value
+    when "foo"
+      p "yes"
+    when "bar"
+      p "no"
+    else
+      p "hiyaa"
+    end
+    CR
+
+    flow = <<-JS
+    const value = "foo";
+    switch (value) {
+    case "foo":
+      console.log("yes");
+      break;
+    case "bar":
+      console.log("no");
+      break;
+    default:
+      console.log("hiyaa");
+      break;
+    }
+    JS
+
+    Crow.convert(crystal).should eq flow
+  end
+
+  it "should raise an exception when conditionless case statements are found" do
+    crystal = <<-CR
+    case
+    when true
+      p "yes"
+    when false
+      p "no"
+    else
+      p "hiyaa"
+    end
+    CR
+
+    exception_raised = false
+
+    begin
+      Crow.convert(crystal)
+    rescue e
+      exception_raised = true
+      e.message.should match /Case statements without conditions cannot be transpiled./
+      e.message.should match %r{See https://github.com/geppetto-apps/crow/issues/8/}
+    ensure
+      Crow.strict = false
+    end
+
+    exception_raised.should eq true
+  end
+end
--- src/crow/conditionals.cr
+++ src/crow/conditionals.cr
@@ -34,5 +34,38 @@ module Crow
       end
       code
     end
+
+    private def transpile(node : Crystal::Case)
+      node_cond = node.cond
+
+      case node_cond
+      when Crystal::ASTNode
+        code = node.whens.map do |when_node|
+          transpile_to_case(when_node)
+        end.join("")
+        code += transpile_to_default(node.else)
+
+        "switch (#{transpile(node_cond)}) {\n#{code}}"
+      else
+        raise "Case statements without conditions cannot be transpiled." \
+        "See https://github.com/geppetto-apps/crow/issues/8/"
+      end
+    end
+
+    private def transpile_to_case(node : Crystal::When)
+      code = node.conds.map do |cond|
+        "case #{transpile(cond)}:"
+      end.join("\n")
+
+      code += format_body(transpile(node.body) + "\nbreak;")
+    end
+
+    private def transpile_to_default(node : Crystal::ASTNode | Nil)
+      return "" if node.is_a? Nil
+
+      transpiled = transpile(node)
+      formatted = format_body("#{transpiled}\nbreak;")
+      "default:#{formatted}"
+    end
   end
 end
